4bee27ef97a05ca66abc4a9a293b5e1354c7c5d3
Mark Muetzelfeldt <markmuetz@gmail.com>
2015-05-31 17:51:51 +0100

    Initial commit: simple (JSON) proxy.
    Allows browser to load arbitrary JSON through a server side proxy.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Load JSON Via Proxy
+
+Simple application to load JSON using a server-side proxy. Either load JSON through AJAX request, or by directly using /load URL. Request sends URL to flask web application, which retrieves to given (JSON) file and returns it to the browser.
+
+## Installation:
+
+    sudo apt-get install pip virtualenv git
+    git clone https://github.com/markmuetz/flask-local-proxy
+    cd flask-local-proxy
+    virtualenv .env
+    source .env/bin/activate
+    pip install -r requirements.txt
+    python app.py
+
+Then navigate to [127.0.0.1:5000](127.0.0.1:5000).
---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, session, request, jsonify, redirect
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+def _download(url):
+    r = requests.get(url)
+    return r.content
+
+
+@app.route('/load')
+def load():
+    url = request.args.get('url')
+    return render_template('load.html', content=_download(url))
+
+
+@app.route('/_api/load_json')
+def _api():
+    url = request.args.get('url')
+    return jsonify({'content': _download(url)})
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
